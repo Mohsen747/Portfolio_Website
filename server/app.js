22b1a7ae863b9efc0604dccc520fd647a0d351f8
Mohsen747 <mohsen.pajoohesh@gmail.com>
2024-01-06 11:05:37 -0500

    Initial files

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/api", (req, res) => {
+    res.json({"users": ["user1", "user2", "user3"] });
+});
+
+app.listen(3002, () => {
+    console.log("Server is running on port 3002");
+});
